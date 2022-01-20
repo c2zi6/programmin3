b67c056013d167ccd1a867168e4ed1dd027d508c
c2zi6 <85682683+c2zi6@users.noreply.github.com>
2022-01-20 17:16:44 +0400

    Add files via upload
    server

---
+++ programmin3/Les.js
@@ -0,0 +1,80 @@
+class Les extends LivingCreature {
+    constructor(x, y) {        
+        super(x, y, 40);
+        this.d = 0;
+    }
+    getNewCoordinates() {
+        this.directions = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1]
+        ];
+    }
+    eat() {
+        if (this.becomeInfected(lesArr)) {
+            this.getNewCoordinates();
+            let found = this.chooseCell(4);
+            let exact = random(found);
+            if (exact) {
+                let x = exact[0];
+                let y = exact[1];
+                matrix[y][x] = 5;
+                matrix[this.y][this.x] = 0;
+                this.y = y;
+                this.x = x;
+                this.energy += 5
+                this.mul();
+            } else {
+                this.move();
+            }
+        }
+    }
+    mul() {
+        let found = [this.chooseCell(0), this.chooseCell(1)];
+        let exact = random(random(found));
+        if (exact) {
+            var y = exact[1];
+            var x = exact[0];
+            for (var i in grassAr) {
+                if (this.x == grassAr[i].x && this.y == grassAr[i].y) {
+                    grassAr.splice(i, 1);
+                }
+            }
+            matrix[y][x] = 3;
+            lesArr.push(new Les(x, y));
+            this.energy = 10;
+        }
+    }
+    move() {
+        let found = [this.chooseCell(0), this.chooseCell(1)];
+        let exact = random(random(found));
+        if (exact) {
+            let x = exact[0];
+            let y = exact[1];
+            if (matrix[y][x] == 0) {
+                this.d = 0;
+            } else if (matrix[y][x] == 1) {
+                this.d = 1;
+            }
+            matrix[y][x] = 5;
+            matrix[this.y][this.x] = this.d;
+            this.x = x;
+            this.y = y;
+            this.energy--;
+            if (this.energy < 0) {
+                this.die();
+            }
+        } else {
+            this.energy--;
+        }
+    }
+    die() {
+        super.del(lesArr);
+        matrix[this.y][this.x] = 4;
+    }
+}
---
+++ programmin3/Virus.js
@@ -0,0 +1,67 @@
+class Virus extends LivingCreature {
+    constructor(x, y) {
+        super(x, y, 15);
+    }
+    getNewCoordinates() {
+        this.directions = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1]
+        ];
+    }
+    move() {
+        this.getNewCoordinates();
+        var found = this.chooseCell(0);
+        var exact = random(found);
+        if (exact) {
+            let x = exact[0];
+            let y = exact[1];
+            matrix[y][x] = 6;
+            matrix[this.y][this.x] = 0;
+            this.x = x;
+            this.y = y;
+        }
+    }
+    infect() {
+        this.energy--;
+        if (this.energy < 0) {
+            this.die();
+        } else {
+            this.getNewCoordinates();
+            var found = [this.chooseCell(2), this.chooseCell(3), this.chooseCell(5)];
+            var exact = random(random(found));
+            if (exact && Math.random() < 0.3) {
+                let x = exact[0];
+                let y = exact[1];
+                matrix[y][x] = 6;
+                virusArr.push(new Virus(x, y));
+                for (var i in lesArr) {
+                    if (x == lesArr[i].x && y == lesArr[i].y) {
+                        lesArr.splice(i, 1);
+                    }
+                }
+                for (var i in grassEaterArr) {
+                    if (x == grassEaterArr[i].x && y == grassEaterArr[i].y) {
+                        grassEaterArr.splice(i, 1);
+                    }
+                }
+                for (var i in predatorArr) {
+                    if (x == predatorArr[i].x && y == predatorArr[i].y) {
+                        predatorArr.splice(i, 1);
+                    }
+                }
+            } else {
+                this.move();
+            }
+        }
+    }
+    die() {
+        super.del(virusArr);
+        matrix[this.y][this.x] = 0;
+    }
+}
