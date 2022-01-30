d0c46de17821207c8ed872e9ba1c50e355e5e487
c2zi6 <85682683+c2zi6@users.noreply.github.com>
2022-01-30 10:35:11 +0400

    Add files via upload

---
+++ programmin3/LivingCreature.js
@@ -0,0 +1,47 @@
+module.exports = class LivingCreature {
+    constructor(x, y, energy) {
+        this.x = x;
+        this.y = y;
+        this.directions = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1]
+        ];
+        this.energy = energy;
+    }
+    chooseCell(char) {
+        var found = [];
+        for (var i in this.directions) {
+            var x = this.directions[i][0];
+            var y = this.directions[i][1];
+            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
+                if (matrix[y][x] == char) {
+                    found.push(this.directions[i]);
+                }
+            }
+        }
+        return found;
+    }
+    del(Arr) {
+        for (var i in Arr) {
+            if (this.x == Arr[i].x && this.y == Arr[i].y) {
+                Arr.splice(i, 1);
+            }
+        }
+    }
+    becomeInfected(Arr) {
+        if (Math.random() * 500 < 1) {
+            this.del(Arr);
+            matrix[this.y][this.x] = 6;
+            virusArr.push(new Virus(this.x, this.y))
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
---
+++ programmin3/server.js
@@ -0,0 +1,110 @@
+var express = require("express");
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var fs = require("fs");
+
+app.use(express.static("."));
+
+app.get('/', function (req, res) {
+    res.redirect('index.html');
+});
+server.listen(3000, () => {
+    console.log('connected');
+});
+
+var matrix = [];
+function matrixGenerator(matrixSize, grassCount, grassEaterCount, predatorCount, lesCount, lCount){
+    for (let i = 0; i < matrixSize; i++) {
+        matrix[i] = []
+        for (let o = 0; o < matrixSize; o++) { 
+            matrix[i][o] = 0;
+        }
+    }
+    for (let i = 0; i < grassCount; i++) {
+        let x = Math.floor(random(matrixSize));
+        let y = Math.floor(random(matrixSize));
+        matrix[y][x] = 1;
+    }
+    for (let i = 0; i < grassEaterCount; i++) {
+        let x = Math.floor(random(matrixSize));
+        let y = Math.floor(random(matrixSize));
+        matrix[y][x] = 2;
+    }
+    for (let i = 0; i < predatorCount; i++) {
+        let x = Math.floor(random(matrixSize));
+        let y = Math.floor(random(matrixSize));
+        matrix[y][x] = 3;
+    }
+    for (let i = 0; i < lesCount; i++) {
+        let x = Math.floor(random(matrixSize));
+        let y = Math.floor(random(matrixSize));
+        matrix[y][x] = 5;
+    }
+    for (let i = 0; i < lCount; i++) {
+        let x = Math.floor(random(matrixSize));
+        let y = Math.floor(random(matrixSize));
+        matrix[y][x] = 4;
+    }
+}
+matrixGenerator(40, 20, 15, 3, 15, 15);
+
+io.sockets.emit('send matrix', matrix);
+
+var grassAr = [];
+var grassEaterArr = [];
+var predatorArr = [];
+var lesArr = [];
+var virusArr = [];
+
+Grass = require("./Grass");
+GrassEater = require("./GrassEater");
+Les = require("./Les");
+Predator = require("./Predator");
+Virus = require("./Virus");
+
+function createObject() {
+    for (var y = 0; y < matrix.length; y++) {
+        for (var x = 0; x < matrix[y].length; x++) {
+            if (matrix[y][x] == 1) {
+                gr = new Grass(x, y);
+                grassAr.push(gr);
+            } else if (matrix[y][x] == 2) {
+                fr = new GrassEater(x, y);
+                grassEaterArr.push(fr);
+            } else if (matrix[y][x] == 3) {
+                pr = new Predator(x, y);
+                predatorArr.push(pr);
+            } else if (matrix[y][x] == 5) {
+                lr = new Les(x, y);
+                lesArr.push(lr);
+            }
+        }
+    }
+    io.socket.emit('send matrix', matrix);
+}
+
+function game() {
+    for (var i in grassAr) {
+        grassAr[i].mul();
+    }
+    for (var i in predatorArr) {
+        predatorArr[i].eat();
+    }
+    for (var i in grassEaterArr) {
+        grassEaterArr[i].eat();
+    }
+    for (var i in lesArr) {
+        lesArr[i].eat();
+    }
+    for (var i in virusArr) {
+        virusArr[i].infect();
+    }
+    io.sockets.emit("send matrix", matrix);
+}
+
+setInterval(game, 1000);
+
+io.on('connection', function () {
+    createObject();
+})
